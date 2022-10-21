2e8b922cca48f86f08e3bbc0ecff5487d45d68e1
AdminOffice <nattavutmail3@gmail.com>
2022-10-21 11:36:51 +0700

    update router products

--- 26-API/routes/products.js
+++ 26-API/routes/products.js
@@ -4,91 +4,11 @@ const router  = express.Router();
  * สำหรับ  http สามารถดูได้ที่ลิงค์
  * https://tips.thaiware.com/1077.html
  */
-const products = [
-    {
-      "id": 1,
-      "title": "iPhone 9",
-      "description": "An apple mobile which is nothing like apple",
-      "price": 549,
-      "discountPercentage": 12.96,
-      "rating": 4.69,
-      "stock": 94,
-      "brand": "Apple",
-      "category": "smartphones",
-      "thumbnail": "...",
-      "images": ["...", "...", "..."]
-    },
-    {
-      "id": 2,
-      "title": "iPhone 10",
-      "description": "An apple mobile which is nothing like apple",
-      "price": 549,
-      "discountPercentage": 12.96,
-      "rating": 4.69,
-      "stock": 94,
-      "brand": "Apple",
-      "category": "smartphones",
-      "thumbnail": "...",
-      "images": ["...", "...", "..."]
-    },
-    {
-      "id": 3,
-      "title": "iPhone 11",
-      "description": "An apple mobile which is nothing like apple",
-      "price": 549,
-      "discountPercentage": 12.96,
-      "rating": 4.69,
-      "stock": 94,
-      "brand": "Apple",
-      "category": "smartphones",
-      "thumbnail": "...",
-      "images": ["...", "...", "..."]
-    },
-    {
-      "id": 4,
-      "title": "iPhone 12",
-      "description": "An apple mobile which is nothing like apple",
-      "price": 549,
-      "discountPercentage": 12.96,
-      "rating": 4.69,
-      "stock": 94,
-      "brand": "Apple",
-      "category": "smartphones",
-      "thumbnail": "...",
-      "images": ["...", "...", "..."]
-    },
-    {
-      "id": 5,
-      "title": "iPhone 13",
-      "description": "An apple mobile which is nothing like apple",
-      "price": 549,
-      "discountPercentage": 12.96,
-      "rating": 4.69,
-      "stock": 94,
-      "brand": "Apple",
-      "category": "smartphones",
-      "thumbnail": "...",
-      "images": ["...", "...", "..."]
-    },
-    {
-      "id": 6,
-      "title": "iPhone 14",
-      "description": "An apple mobile which is nothing like apple",
-      "price": 549,
-      "discountPercentage": 12.96,
-      "rating": 4.69,
-      "stock": 94,
-      "brand": "Apple",
-      "category": "smartphones",
-      "thumbnail": "...",
-      "images": ["...", "...", "..."]
-    },
-]
 
 router.get('/',(req,res,next)=>{
     res.status(200).json({
         message:"Handling GET requests to /products",
-        response:products
+        response:'true'
     });
 });
 
